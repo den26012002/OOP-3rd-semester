8b853bc2160be80b964559d2a49c672bf3237bb1
den26012002 <den26012002@mail.ru>
2021-11-22 10:35:05 +0300

    feat: add loggers

---
+++ BackupsExtra/Entities/FileLogger.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Text;
+
+namespace BackupsExtra.Entities
+{
+    public class FileLogger : ILogger
+    {
+        private string _logFileName;
+
+        public FileLogger(string logFileName)
+        {
+            _logFileName = logFileName;
+            if (!File.Exists(_logFileName))
+            {
+                File.Create(_logFileName);
+            }
+        }
+
+        public void Log(string message)
+        {
+            using (var ofstream = new FileStream(_logFileName, FileMode.Append))
+            {
+                ofstream.Write(Encoding.Default.GetBytes(message));
+            }
+        }
+    }
+}
---
+++ BackupsExtra/Entities/TimeLoggerDecorator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BackupsExtra.Entities
+{
+    public class TimeLoggerDecorator : ILogger
+    {
+        private ILogger _logger;
+
+        public TimeLoggerDecorator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Log(string message)
+        {
+            _logger.Log(DateTime.Now.ToString() + " " + message);
+        }
+    }
+}
